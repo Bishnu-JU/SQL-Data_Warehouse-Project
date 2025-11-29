e082045855993b4eceb99d1f73ad57eb68ccecd6
Bishnu-JU <imbishnu2001@gmail.com>
2025-11-30 01:05:13 +0530

    changes committed

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -86,10 +86,11 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE bronze.erp_px_cat_g12(
+CREATE TABLE bronze.erp_px_cat_g1v2(
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
 	maintenance NVARCHAR(50)
 );
+
 GO
